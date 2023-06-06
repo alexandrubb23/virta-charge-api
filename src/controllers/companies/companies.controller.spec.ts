12660cb6ab17330c398ead4aafcf0306c9c213f6
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2023-06-06 13:20:26 +0300

    Test company crud operations.

--- src/controllers/companies/companies.controller.spec.ts
+++ src/controllers/companies/companies.controller.spec.ts
@@ -15,4 +15,38 @@ describe('CompaniesController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  describe('CRUD operations', () => {
+    it('should return all companies', () => {
+      expect(controller.findAll()).toBe('This action returns all companies');
+    });
+
+    it('should return a company', () => {
+      expect(controller.findOne('1')).toBe('This action returns a #1 company');
+    });
+
+    it('should create a company', () => {
+      const body = {
+        name: 'Company 1',
+        parent_company_id: 0,
+      };
+
+      expect(controller.create(body)).toEqual(body);
+    });
+
+    it('should update a company', () => {
+      const body = {
+        name: 'Company 1',
+        parent_company_id: 0,
+      };
+
+      expect(controller.update('1', body)).toBe(
+        'This action updates a #1 company',
+      );
+    });
+
+    it('should delete a company', () => {
+      expect(controller.remove('1')).toBe('This action removes a #1 company');
+    });
+  });
 });
