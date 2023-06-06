eb5d587d17a57488f2ceb541b49efe4ecf8404c4
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2023-06-06 12:56:23 +0300

    Implement find company based on an id.

--- src/controllers/companies/companies.controller.ts
+++ src/controllers/companies/companies.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('companies')
 export class CompaniesController {
   @Get()
   findAll(): string {
     return 'This action returns all companies';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return `This action returns a #${id} company`;
   }
 }
