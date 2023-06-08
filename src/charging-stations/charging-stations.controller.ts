25890ca27c1edc90deeedcb42444dfca0c9d958c
Barbulescu Alexandru <alex_bb23@yahoo.co.uk>
2023-06-08 15:34:53 +0300

    Implement update charging station.

--- src/charging-stations/charging-stations.controller.ts
+++ src/charging-stations/charging-stations.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { ChargingStationsService } from './charging-stations.service';
 import { CreateChargingStationDto } from './dto/create-charging-station.dto';
 import { ChargingStation } from './entities/charging-station.entity';
+import { UpdateChargingStationDto } from './dto/update-charging-station.dto';
 
 @Controller('charging-stations')
 export class ChargingStationsController {
@@ -23,5 +24,13 @@ export class ChargingStationsController {
     @Body() createChargingStationDto: CreateChargingStationDto,
   ): Promise<ChargingStation> {
     return this.chargingStationService.create(createChargingStationDto);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: number,
+    @Body() updateChargingStationDto: UpdateChargingStationDto,
+  ): Promise<ChargingStation> {
+    return this.chargingStationService.update(id, updateChargingStationDto);
   }
 }
--- src/charging-stations/charging-stations.service.ts
+++ src/charging-stations/charging-stations.service.ts
@@ -4,11 +4,12 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { DataSource, Repository } from 'typeorm';
+import { DataSource, FindOneOptions, Repository } from 'typeorm';
 
 import { ChargingStation } from './entities/charging-station.entity';
 import { CreateChargingStationDto } from './dto/create-charging-station.dto';
 import { Company } from 'src/companies/entities/company.entity';
+import { UpdateChargingStationDto } from './dto/update-charging-station.dto';
 
 @Injectable()
 export class ChargingStationsService {
@@ -25,35 +26,31 @@ export class ChargingStationsService {
   }
 
   async findOne(id: number): Promise<ChargingStation> {
-    const options = {
+    const options: FindOneOptions<ChargingStation> = {
       where: { id },
     };
 
-    const charginStation = await this.chargingStationsRepository.findOne(
+    const chargingStation = await this.chargingStationsRepository.findOne(
       options,
     );
 
-    if (!charginStation) {
+    if (!chargingStation) {
       throw new NotFoundException(`Charging Station #${id} not found`);
     }
 
-    return charginStation;
+    return chargingStation;
   }
 
   async create(
     createChargingStationDto: CreateChargingStationDto,
   ): Promise<ChargingStation> {
-    const options = {
-      where: { id: createChargingStationDto.company_id },
-      relations: {
-        charging_stations: true,
-      },
-    };
-
-    const company = await this.companiesRepository.findOne(options);
+    const { company_id } = createChargingStationDto;
+    const company = await this.findCompany(company_id);
 
     if (!company) {
-      throw new BadRequestException('Company with the given id not found');
+      throw new BadRequestException(
+        `Charging Station #${company_id} not found`,
+      );
     }
 
     const chargingStation = this.chargingStationsRepository.create(
@@ -74,8 +71,48 @@ export class ChargingStationsService {
       return chargingStation;
     } catch (error) {
       await queryRunner.rollbackTransaction();
+      throw error;
     } finally {
       await queryRunner.release();
     }
+  }
+
+  async update(id: number, updateChargingStationDto: UpdateChargingStationDto) {
+    if (Object.keys(updateChargingStationDto).length === 0) {
+      throw new BadRequestException('No fields to update were provided');
+    }
+
+    const company = await this.findCompany(updateChargingStationDto.company_id);
+    if (!company) {
+      throw new BadRequestException(`Company #${id} not found`);
+    }
+
+    const options: FindOneOptions<ChargingStation> = {
+      where: { id },
+    };
+
+    const chargingStation = await this.chargingStationsRepository.findOne(
+      options,
+    );
+
+    if (!chargingStation) {
+      throw new BadRequestException(`Charging Station #${id} not found`);
+    }
+
+    const updatedChargingStation = await this.chargingStationsRepository.save({
+      ...chargingStation,
+      ...updateChargingStationDto,
+    });
+
+    return updatedChargingStation;
+  }
+
+  private findCompany(id: number): Promise<Company> {
+    const options = {
+      where: { id },
+      relations: ['charging_stations'],
+    };
+
+    return this.companiesRepository.findOne(options);
   }
 }
