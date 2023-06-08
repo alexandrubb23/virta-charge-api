0167a1ca8054a76abecf36a2693b7fcd0e6c9546
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2023-06-08 13:55:36 +0300

    Implement charging-stations entity.

---
+++ src/charging-stations/entities/charging-station.entity.ts
@@ -0,0 +1,41 @@
+import { Company } from 'src/companies/entities/company.entity';
+import {
+  Column,
+  Entity,
+  Index,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class ChargingStation {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({
+    type: 'decimal',
+    precision: 10,
+    scale: 6,
+  })
+  latitude: number;
+
+  @Column({
+    type: 'decimal',
+    precision: 10,
+    scale: 6,
+  })
+  longitude: number;
+
+  @Index()
+  @Column()
+  company_id: number;
+
+  @Column()
+  address: string;
+
+  @ManyToMany(() => Company, (company) => company.charging_stations)
+  company: Company;
+}
