e9a060700c316b55bdb2a65df879a8e6b0113261
Barbulescu Alexandru <alex_bb23@yahoo.co.uk>
2023-06-09 18:43:08 +0300

    Remove database config.

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,6 @@ import { AppService } from './app.service';
 import { ChargingStationsModule } from './charging-stations/charging-stations.module';
 import { CompaniesModule } from './companies/companies.module';
 import { databaseSchema } from './validation';
-import { databaseConfig } from './config';
 import isProduction from './utils/environment';
 
 @Module({
@@ -18,7 +17,18 @@ import isProduction from './utils/environment';
     }),
     CompaniesModule,
     ChargingStationsModule,
-    TypeOrmModule.forRoot(databaseConfig()),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_DB,
+        autoLoadEntities: !isProduction,
+        synchronize: !isProduction,
+      }),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
