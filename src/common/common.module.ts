6fa881d35a79ef07585905fc4c7f4b205b63f6aa
Barbulescu Alexandru <alex_bb23@yahoo.co.uk>
2023-06-10 18:29:56 +0300

    Implement logging middleware.

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 
 import { ApiKeyGuard } from './guards/api-key/api-key.guard';
 import { TimeoutInterceptor } from './interceptors/timeout.interceptor';
+import { LoggingMiddleware } from './middleware/logging.middleware';
 
 @Module({
   imports: [ConfigModule],
@@ -18,4 +19,8 @@ import { TimeoutInterceptor } from './interceptors/timeout.interceptor';
     },
   ],
 })
-export class CommonModule {}
+export class CommonModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggingMiddleware).forRoutes('*');
+  }
+}
