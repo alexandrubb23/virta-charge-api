e36b068131f7648faea0a449210ab2c01b3361aa
Barbulescu Alexandru <alex_bb23@yahoo.co.uk>
2023-06-10 13:46:08 +0300

    Refactor companiesWithChargingStations.

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -111,31 +111,56 @@ export class CompaniesService {
   }
 
   private companiesWithChargingStations(companies: Company[]): Company[] {
-    const chargingStations = (company: Company, allCompanies: Company[]) => {
-      if (company.parentId === company.id) return [];
+    const procesed = new Map();
+    const companiesWithChargingStations = new Map();
 
-      const children = allCompanies.filter((c) => c.parentId === company.id);
-      if (children.length === 0) {
-        return company.charging_stations;
-      }
+    companies.forEach((company) => {
+      companiesWithChargingStations.set(company.id, company);
+    });
 
-      return children.flatMap((childCompany) => [
-        ...childCompany.charging_stations,
-        ...chargingStations(childCompany, allCompanies),
-      ]);
+    const childrenChargingStations = (company, parents = new Set()) => {
+      if (procesed.get(company.id)) return;
+
+      // The time complexity of this function is O(n) where n is the number of companies
+      const children = companies.filter(
+        (c) => c.parentId === company.id && c.id !== c.parentId,
+      );
+
+      if (children.length === 0) return;
+
+      // The time complexity of this function is O(1) on average
+      parents.add(company.id);
+
+      // The time complexity of this operation is O(1)
+      // since it's accessing an array element by index
+      const nextChild = children[0];
+      if (!nextChild) return;
+
+      // The time complexity of this operation is O(k),
+      // where k is the total number of charging stations across all children
+      const mappedChildren = children.flatMap((c) => c.charging_stations);
+
+      // The time complexity of this loop is O(p),
+      // where p is the number of parents in the set
+      parents.forEach((parent) => {
+        procesed.set(company.id, true);
+
+        const getCompany = companiesWithChargingStations.get(parent);
+
+        companiesWithChargingStations.set(parent, {
+          ...getCompany,
+          charging_stations: [
+            ...getCompany.charging_stations,
+            ...mappedChildren,
+          ],
+        });
+      });
+
+      return childrenChargingStations(nextChild, parents);
     };
 
-    return companies.map((company) => ({
-      ...company,
-      charging_stations: [
-        ...company.charging_stations,
-        ...chargingStations(company, companies),
-      ].filter((chargingStation, index, self) => {
-        return (
-          chargingStation &&
-          index === self.findIndex((c) => c.id === chargingStation.id)
-        );
-      }),
-    }));
+    companies.forEach((company) => childrenChargingStations(company));
+
+    return Array.from(companiesWithChargingStations.values());
   }
 }
