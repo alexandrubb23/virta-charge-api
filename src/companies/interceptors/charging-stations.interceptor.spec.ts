5fc79ea18ac9620d9c55155904d060a3c4a5931c
Barbulescu Alexandru <alex_bb23@yahoo.co.uk>
2023-06-10 17:30:46 +0300

    Implement charging stations interceptor.

---
+++ src/companies/interceptors/charging-stations.interceptor.spec.ts
@@ -0,0 +1,7 @@
+import { CompanyChargingStationsInterceptor } from './company-charging-stations.interceptor';
+
+describe('ChargingStationsInterceptorInterceptor', () => {
+  it('should be defined', () => {
+    expect(new CompanyChargingStationsInterceptor()).toBeDefined();
+  });
+});
