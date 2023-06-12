43c7e94fbb2d7501de718caebdeb1dafa16d3452
Barbulescu Alexandru <alex_bb23@yahoo.co.uk>
2023-06-12 09:59:03 +0300

    Implement createSwaggerDocument.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,28 @@
-import { ValidationPipe } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+
+function createSwaggerDocument(app: INestApplication): void {
+  const options = new DocumentBuilder()
+    .addBearerAuth({
+      description: 'Enter your API key',
+      name: 'Authorization',
+      bearerFormat: 'Bearer',
+      scheme: 'Bearer',
+      type: 'http',
+      in: 'Header',
+    })
+    .setTitle('The Venus Project API')
+    .setDescription('The Venus Project API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -21,24 +40,9 @@ async function bootstrap() {
 
   app.useGlobalFilters(new HttpExceptionFilter());
 
-  const options = new DocumentBuilder()
-    .addBearerAuth({
-      description: 'Enter your API key',
-      name: 'Authorization',
-      bearerFormat: 'Bearer',
-      scheme: 'Bearer',
-      type: 'http',
-      in: 'Header',
-    })
-    .setTitle('The Venus Project API')
-    .setDescription('The Venus Project API description')
-    .setVersion('1.0')
-    .build();
+  createSwaggerDocument(app);
 
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
-
-  await app.listen(3000);
+  await app.listen(process.env.API_PORT || 3000);
 }
 
 bootstrap();
