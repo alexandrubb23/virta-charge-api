a23177a56ffa1dc1d38caf4c2076c3ba2d8e19f9
Barbulescu Alexandru <alex_bb23@yahoo.co.uk>
2023-06-12 17:32:11 +0300

    Update companies service spec.

--- src/companies/companies.service.spec.ts
+++ src/companies/companies.service.spec.ts
@@ -1,55 +1,28 @@
 import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 
-import { ChargingStation } from 'src/charging-stations/entities/charging-station.entity';
-import { CompaniesService } from './companies.service';
-import { Company } from './entities/company.entity';
-import { CreateCompanyDto } from './dto/create-company.dto';
 import { DataService } from 'src/common/repository/data-service';
-import { GenericRepository } from 'src/common/repository/generic-repository';
+import {
+  createMockRepository,
+  spyOnChargingStationsService,
+  spyOnCompaniesService,
+} from 'test/utils/mock.repositoy';
+import { CompaniesService } from './companies.service';
+import { CreateCompanyDto } from './dto/create-company.dto';
 import { UpdateCompanyDto } from './dto/update-company.dto';
-
-const createMockGenericRepository = <T = any>(): GenericRepository<T> =>
-  ({
-    findOne: jest.fn(),
-    create: jest.fn(),
-    save: jest.fn(),
-    preload: jest.fn(),
-    remove: jest.fn(),
-    findAll: jest.fn(),
-    update: jest.fn(),
-    findNearbyChargingStations: jest.fn(),
-  } as unknown as GenericRepository<T>);
-
-const createMockRepository = (): DataService => ({
-  companies: createMockGenericRepository<Company>(),
-  chargingStations: createMockGenericRepository<ChargingStation>(),
-});
-
-type SpyOn = {
-  methodName: keyof DataService['companies'];
-  value: Company | Company[];
-};
+import { expectNotFoundException } from 'test/utils/expect.exception';
 
 describe('CompaniesService', () => {
   let service: CompaniesService;
   let dataService: DataService;
 
-  const spyOnCompaniesServiceMethod = ({ methodName, value }: SpyOn) => {
-    jest.spyOn(dataService.companies, methodName).mockResolvedValue(value);
-  };
+  const companyId = 1;
 
-  const expectNotFoundException = async (
-    action: () => Promise<any>,
-    companyId: number,
-  ) => {
-    try {
-      await action();
-      expect(false).toBeTruthy(); // we should not get here
-    } catch (error) {
-      expect(error).toBeInstanceOf(NotFoundException);
-      expect(error.message).toEqual(`Company #${companyId} not found`);
-    }
+  const expectedCompany = {
+    id: 1,
+    name: 'Company 1',
+    parentId: 0,
+    charging_stations: [],
   };
 
   beforeEach(async () => {
@@ -58,7 +31,7 @@ describe('CompaniesService', () => {
         CompaniesService,
         {
           provide: DataService,
-          useValue: createMockRepository() as jest.Mocked<DataService>,
+          useValue: createMockRepository(),
         },
       ],
     }).compile();
@@ -67,22 +40,18 @@ describe('CompaniesService', () => {
     dataService = module.get<DataService>(DataService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  let spyOnCompanies;
+  let spyOnChargingStations;
+  beforeEach(() => {
+    spyOnCompanies = spyOnCompaniesService(dataService);
+    spyOnChargingStations = spyOnChargingStationsService(dataService);
   });
 
   describe('findAll /', () => {
     it('should return an array of companies', async () => {
-      const expectedCompanies = [
-        {
-          id: 1,
-          name: 'Company 1',
-          parentId: 0,
-          charging_stations: [],
-        },
-      ];
+      const expectedCompanies = [expectedCompany];
 
-      spyOnCompaniesServiceMethod({
+      spyOnCompanies({
         methodName: 'findAll',
         value: expectedCompanies,
       });
@@ -95,15 +64,7 @@ describe('CompaniesService', () => {
   describe('findOne /', () => {
     describe('when company with ID exists', () => {
       it('should return the company object', async () => {
-        const companyId = 1;
-        const expectedCompany = {
-          id: 1,
-          name: 'Company 1',
-          parentId: 0,
-          charging_stations: [],
-        };
-
-        spyOnCompaniesServiceMethod({
+        spyOnCompanies({
           methodName: 'findOne',
           value: expectedCompany,
         });
@@ -115,13 +76,11 @@ describe('CompaniesService', () => {
 
     describe('otherwise', () => {
       it('should throw the "NotFoundException"', async () => {
-        const companyId = 1;
-
-        expect(() => service.findOne(companyId)).rejects.toThrowError(
+        await expectNotFoundException(
+          () => service.findOne(companyId),
+          1,
           `Company #${companyId} not found`,
         );
-
-        await expectNotFoundException(() => service.findOne(companyId), 1);
       });
     });
   });
@@ -133,14 +92,7 @@ describe('CompaniesService', () => {
         parentId: 0,
       };
 
-      const expectedCompany = {
-        id: 1,
-        name: 'Company 1',
-        parentId: 0,
-        charging_stations: [],
-      };
-
-      spyOnCompaniesServiceMethod({
+      spyOnCompanies({
         methodName: 'save',
         value: expectedCompany,
       });
@@ -153,30 +105,22 @@ describe('CompaniesService', () => {
   describe('update /', () => {
     describe('when company with ID exists', () => {
       it('should update the company object', async () => {
-        const companyId = 1;
         const updateCompanyDto: UpdateCompanyDto = {
           name: 'Company 1',
           parentId: 0,
         };
 
-        const expectedCompany = {
-          id: 1,
-          name: 'Company 1',
-          parentId: 0,
-          charging_stations: [],
-        };
-
-        spyOnCompaniesServiceMethod({
+        spyOnCompanies({
           methodName: 'findOne',
           value: expectedCompany,
         });
 
-        spyOnCompaniesServiceMethod({
+        spyOnCompanies({
           methodName: 'save',
           value: expectedCompany,
         });
 
-        spyOnCompaniesServiceMethod({
+        spyOnCompanies({
           methodName: 'preload',
           value: expectedCompany,
         });
@@ -197,6 +141,7 @@ describe('CompaniesService', () => {
         await expectNotFoundException(
           () => service.update(companyId, updateCompanyDto),
           1,
+          `Company #${companyId} not found`,
         );
       });
     });
@@ -205,21 +150,12 @@ describe('CompaniesService', () => {
   describe('remove /', () => {
     describe('when company with ID exists', () => {
       it('should remove the company object', async () => {
-        const companyId = 1;
-
-        const expectedCompany = {
-          id: 1,
-          name: 'Company 1',
-          parentId: 0,
-          charging_stations: [],
-        };
-
-        spyOnCompaniesServiceMethod({
+        spyOnCompanies({
           methodName: 'findOne',
           value: expectedCompany,
         });
 
-        spyOnCompaniesServiceMethod({
+        spyOnCompanies({
           methodName: 'remove',
           value: expectedCompany,
         });
@@ -234,12 +170,16 @@ describe('CompaniesService', () => {
       it('should throw the "NotFoundException"', async () => {
         const companyId = 1;
 
-        spyOnCompaniesServiceMethod({
+        spyOnCompanies({
           methodName: 'findOne',
           value: undefined,
         });
 
-        await expectNotFoundException(() => service.remove(companyId), 1);
+        await expectNotFoundException(
+          () => service.remove(companyId),
+          1,
+          `Company #${companyId} not found`,
+        );
       });
     });
   });
