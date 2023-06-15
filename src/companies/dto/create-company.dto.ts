85fc057bbfd20f5bf29986e0a3acb703c7437a05
Barbulescu Alexandru <alex_bb23@yahoo.co.uk>
2023-06-15 16:17:28 +0300

    Apply IsPositiveOrZero on create company DTO.

--- src/companies/dto/create-company.dto.ts
+++ src/companies/dto/create-company.dto.ts
@@ -1,5 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { IsPositiveOrZero } from 'src/common/decorators/is-positive-or-zero.decorator';
 
 export class CreateCompanyDto {
   @ApiProperty({ description: 'The name of the company' })
@@ -8,6 +9,7 @@ export class CreateCompanyDto {
   readonly name: string;
 
   @ApiProperty({ description: 'The parent company id' })
+  @IsPositiveOrZero()
   @IsNumber()
   readonly parentId: number;
 }
